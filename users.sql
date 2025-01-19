552d2934e4e8a500f32105a228cedbf3c4d73894
Juan Isaza <juanisaza@Juans-MacBook-Pro.local>
2025-01-18 22:04:38 -0500

    feat(users): adds base users

---
+++ users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(30) UNIQUE,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
