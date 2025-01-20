4c8a242b1b74fa4a0c91a5a5d4cf810e51fe4e8d
Juan Isaza <juanisaza@Juans-MacBook-Pro.local>
2025-01-19 22:07:28 -0500

    feat(family): adds family members

---
+++ familiares.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Familiares (
+    id_acudiente INT AUTO_INCREMENT PRIMARY KEY,
+    n_documento VARCHAR(255) NOT NULL,
+    nombres VARCHAR(50) NOT NULL,
+    apellidos VARCHAR(50) NOT NULL,
+    telefono VARCHAR(50),
+    direccion VARCHAR(255),
+    email VARCHAR(50) UNIQUE,
+    acudiente BOOL,
+    vive BOOL
+);
--- familiares_y_acudientes_por_usuario.sql
+++ familiares_y_acudientes_por_usuario.sql
@@ -1,9 +1,15 @@
-CREATE TABLE familiares_y_acudientes_por_usuario(
-    id_acudiente INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE familiares_y_acudientes_por_usuario (
+    id_acudiente INT NOT NULL,
     id_usuario INT NOT NULL,
     parentesco ENUM('Padre', 'Madre', 'Hermano', 'Hermana', 'Tío', 'Tía', 'Primo', 'Prima', 'Amigo', 'Otro') NOT NULL,
-
-    CONSTRAINT fk_acudiente_usuario FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario)
+    
+    PRIMARY KEY (id_acudiente, id_usuario),
+    
+    CONSTRAINT fk_acudiente FOREIGN KEY (id_acudiente) REFERENCES Familiares(id_acudiente)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    
+    CONSTRAINT fk_usuario FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario)
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
